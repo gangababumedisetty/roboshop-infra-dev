8b3842e73d059c1f8d6ac5f398321e07fb6da9d3
gangababu M <gangababumedisetty.com>
2026-03-21 12:41:08 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -9,4 +9,4 @@ git clone https://github.com/gangababumedisetty/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
 git pull
-ansible-playbook -i inventory.ini -e component=$component -e env=$environment roboshop.yaml
+ansible-playbook -e component=$component -e env=$environment roboshop.yaml
