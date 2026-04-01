9d511f39fd077d48b078b1746160b631b4e7bbaf
gangababu M <gangababumedisetty.com>
2026-04-01 18:53:09 +0530

    terraform

--- 90-components/bootstrap.sh
+++ 90-components/bootstrap.sh
@@ -3,6 +3,7 @@
 component=$1
 environment=$2
 app_version=$3
+DOMAIN=$4
 dnf install ansible -y
 
 cd /home/ec2-user
@@ -10,4 +11,4 @@ git clone https://github.com/gangababumedisetty/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
 git pull
-ansible-playbook -e component=$component -e env=$environment -e app_version=$app_version roboshop.yaml
+ansible-playbook -e component=$component -e env=$environment -e app_version=$app_version  -e domain=$DOMAIN roboshop.yaml
