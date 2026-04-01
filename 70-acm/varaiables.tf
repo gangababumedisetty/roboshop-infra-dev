69a19a8806fc7964b703df03b3d0a5a486ef68bc
gangababu M <gangababumedisetty.com>
2026-04-01 17:27:39 +0530

    terraform

---
+++ 70-acm/varaiables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z08902251BPAS0UIU9K4C"
+}
+
+variable "domain_name" {
+    default  = "gbdaws88s.online"
+}
